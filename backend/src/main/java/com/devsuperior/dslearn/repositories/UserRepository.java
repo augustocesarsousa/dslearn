8917cfbd595c0e8ebc066f5b6abb43228b9f33ab
augusto cesar <acs-03@hotmail.com>
2021-10-21 21:52:52 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearn.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
