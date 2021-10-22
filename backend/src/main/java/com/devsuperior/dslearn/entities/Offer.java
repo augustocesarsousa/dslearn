95df2e1e66144865466b8691a838eaedb31dfa99
augusto cesar <acs-03@hotmail.com>
2021-10-21 21:33:30 -0300

    Fix: missing associations, getters

--- backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -41,6 +41,9 @@ public class Offer implements Serializable{
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();
+	
 	public Offer() {
 		
 	}	
@@ -96,6 +99,10 @@ public class Offer implements Serializable{
 
 	public List<Resource> getResources() {
 		return resources;
+	}
+	
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
--- backend/src/main/java/com/devsuperior/dslearn/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/User.java
@@ -44,13 +44,12 @@ public class User implements Serializable{
 		
 	}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+	public User(Long id, String name, String email, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
 	}
 
 	public Long getId() {
@@ -88,10 +87,6 @@ public class User implements Serializable{
 	public Set<Role> getRoles() {
 		return roles;
 	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}	
 
 	public List<Notification> getNotifications() {
 		return notifications;
